7d8c7843fdcfefdb6a86ca2034a92fce5b9ee71c
Anurag Kejriwal <106197059+anuragkej@users.noreply.github.com>
2023-09-03 23:01:41 -0400

    Add files via upload
    Uploaded files from the Zip file for easier access.

---
+++ mapdata.js
@@ -0,0 +1,565 @@
+var simplemaps_usmap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes", 
+    border_color: "#ffffff",
+    popups: "detect",
+    
+    //State defaults
+		state_description: "",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "",
+    location_color: "#2041D4",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+    manual_zoom: "no",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: -1,
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "yes",
+    images_directory: "/static/lib/simplemaps/map_images/",
+    fade_time: 0.1,
+    import_labels: "no",
+    link_text: "View Website",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    state_description:"",
+    HI: {
+      name: "Hawaii: 6650 Thousand Metric Tons"
+    },
+    AK: {
+      name: "Alaska: 3527 Thousand Metric Tons"
+    },
+    FL: {
+      name: "Florida: 96975 Thousand Metric Tons"
+    },
+    NH: {
+      name: "New Hampshire: 1734 Thousand Metric Tons"
+    },
+    VT: {
+      name: "Vermont: 8 Thousand Metric Tons"
+    },
+    ME: {
+      name: "Maine: 1831 Thousand Metric Tons"
+    },
+    RI: {
+      name: "Rhode Island: 3367 Thousand Metric Tons"
+    },
+    NY: {
+      name: "New York: 26849 Thousand Metric Tons"
+    },
+    PA: {
+      name: "Pennsylvania: 72459 Thousand Metric Tons"
+    },
+    NJ: {
+      name: "New Jersey: 14942 Thousand Metric Tons"
+    },
+    DE: {
+      name: "Delaware: 2675 Thousand Metric Tons"
+    },
+    MD: {
+      name: "Maryland: 10241 Thousand Metric Tons"
+    },
+    VA: {
+      name: "Virginia: 31893 Thousand Metric Tons"
+    },
+    WV: {
+      name: "West Virginia: 49588 Thousand Metric Tons"
+    },
+    OH: {
+      name: "Ohio: 67350 Thousand Metric Tons"
+    },
+    IN: {
+      name: "Indiana: 64884 Thousand Metric Tons"
+    },
+    IL: {
+      name: "Illinois: 47595 Thousand Metric Tons"
+    },
+    CT: {
+      name: "Connecticut: 10215 Thousand Metric Tons"
+    },
+    WI: {
+      name: "Wisconsin: 33233 Thousand Metric Tons"
+    },
+    NC: {
+      name: "North Carolina: 38542 Thousand Metric Tons"
+    },
+    DC: {
+      name: "District of Columbia: 108 Thousand Metric Tons"
+    },
+    MA: {
+      name: "Massachusetts: 7978 Thousand Metric Tons"
+    },
+    TN: {
+      name: "Tennessee: 22823 Thousand Metric Tons"
+    },
+    AR: {
+      name: "Arkansas: 23504 Thousand Metric Tons"
+    },
+    MO: {
+      name: "Missouri: 54206 Thousand Metric Tons"
+    },
+    GA: {
+      name: "Georgia 39957 Thousand Metric Tons"
+    },
+    SC: {
+      name: "South Carolina: 23131 Thousand Metric Tons"
+    },
+    KY: {
+      name: "Kentucky: 49825 Thousand Metric Tons"
+    },
+    AL: {
+      name: "Alabama: 44895 Thousand Metric Tons"
+    },
+    LA: {
+      name: "Louisiana: 44525 Thousand Metric Tons"
+    },
+    MS: {
+      name: "Mississippi: 26815 Thousand Metric Tonsa"
+    },
+    IA: {
+      name: "Iowa: 21164 Thousand Metric Tons"
+    },
+    MN: {
+      name: "Minnesota: 20986 Thousand Metric Tons"
+    },
+    OK: {
+      name: "Oklahoma: 25879 Thousand Metric Tons"
+    },
+    TX: {
+      name: "Texas: 202830 Thousand Metric Tons"
+    },
+    NM: {
+      name: "New Mexico: 18687 Thousand Metric Tons"
+    },
+    KS: {
+      name: "Kansas: 20363 Thousand Metric Tons"
+    },
+    NE: {
+      name: "Nebraska: 20967 Thousand Metric Tons"
+    },
+    SD: {
+      name: "South Dakota: 2365 Thousand Metric Tons"
+    },
+    ND: {
+      name: "North Dakota: 27437 Thousand Metric Tons"
+    },
+    WY: {
+      name: "Wyoming: 37594 Thousand Metric Tons"
+    },
+    MT: {
+      name: "Montana: 10421 Thousand Metric Tons"
+    },
+    CO: {
+      name: "Colorado: 29113 Thousand Metric Tons"
+    },
+    UT: {
+      name: "Utah: 26342 Thousand Metric Tons"
+    },
+    AZ: {
+      name: "Arizona: 35791 Thousand Metric Tons"
+    },
+    NV: {
+      name: "Nevada: 13584 Thousand Metric Tons"
+    },
+    OR: {
+      name: "Oregon: 9401 Thousand Metric Tons"
+    },
+    WA: {
+      name: "Washington: 12035 Thousand Metric Tons"
+    },
+    CA: {
+      name: "California: 43440 Thousand Metric Tons"
+    },
+    MI: {
+      name: "Michigan: 53285 Thousand Metric Tons"
+    },
+    ID: {
+      name: "Idaho: 2230 Thousand Metric Tons"
+    },
+    GU: {
+      name: "Guam: ",
+      hide: "yes"
+    },
+    VI: {
+      name: "Virgin Islands",
+      hide: "yes"
+    },
+    PR: {
+      name: "Puerto Rico",
+      hide: "yes"
+    },
+    MP: {
+      name: "Northern Mariana Islands",
+      hide: "yes"
+    },
+    AS: {
+      name: "American Samoa",
+      hide: "yes"
+    }
+  },
+  locations: {
+
+  },
+  labels: {
+    NH: {
+      parent_id: "NH",
+      x: "932",
+      y: "183",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    VT: {
+      parent_id: "VT",
+      x: "883",
+      y: "243",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    RI: {
+      parent_id: "RI",
+      x: "932",
+      y: "273",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    NJ: {
+      parent_id: "NJ",
+      x: "883",
+      y: "273",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    DE: {
+      parent_id: "DE",
+      x: "883",
+      y: "303",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    MD: {
+      parent_id: "MD",
+      x: "932",
+      y: "303",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    DC: {
+      parent_id: "DC",
+      x: "884",
+      y: "332",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    MA: {
+      parent_id: "MA",
+      x: "932",
+      y: "213",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    CT: {
+      parent_id: "CT",
+      x: "932",
+      y: "243",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    HI: {
+      parent_id: "HI",
+      x: 305,
+      y: 565,
+      pill: "yes"
+    },
+    AK: {
+      parent_id: "AK",
+      x: "113",
+      y: "495"
+    },
+    FL: {
+      parent_id: "FL",
+      x: "773",
+      y: "510"
+    },
+    ME: {
+      parent_id: "ME",
+      x: "893",
+      y: "85"
+    },
+    NY: {
+      parent_id: "NY",
+      x: "815",
+      y: "158"
+    },
+    PA: {
+      parent_id: "PA",
+      x: "786",
+      y: "210"
+    },
+    VA: {
+      parent_id: "VA",
+      x: "790",
+      y: "282"
+    },
+    WV: {
+      parent_id: "WV",
+      x: "744",
+      y: "270"
+    },
+    OH: {
+      parent_id: "OH",
+      x: "700",
+      y: "240"
+    },
+    IN: {
+      parent_id: "IN",
+      x: "650",
+      y: "250"
+    },
+    IL: {
+      parent_id: "IL",
+      x: "600",
+      y: "250"
+    },
+    WI: {
+      parent_id: "WI",
+      x: "575",
+      y: "155"
+    },
+    NC: {
+      parent_id: "NC",
+      x: "784",
+      y: "326"
+    },
+    TN: {
+      parent_id: "TN",
+      x: "655",
+      y: "340"
+    },
+    AR: {
+      parent_id: "AR",
+      x: "548",
+      y: "368"
+    },
+    MO: {
+      parent_id: "MO",
+      x: "548",
+      y: "293"
+    },
+    GA: {
+      parent_id: "GA",
+      x: "718",
+      y: "405"
+    },
+    SC: {
+      parent_id: "SC",
+      x: "760",
+      y: "371"
+    },
+    KY: {
+      parent_id: "KY",
+      x: "680",
+      y: "300"
+    },
+    AL: {
+      parent_id: "AL",
+      x: "655",
+      y: "405"
+    },
+    LA: {
+      parent_id: "LA",
+      x: "550",
+      y: "435"
+    },
+    MS: {
+      parent_id: "MS",
+      x: "600",
+      y: "405"
+    },
+    IA: {
+      parent_id: "IA",
+      x: "525",
+      y: "210"
+    },
+    MN: {
+      parent_id: "MN",
+      x: "506",
+      y: "124"
+    },
+    OK: {
+      parent_id: "OK",
+      x: "460",
+      y: "360"
+    },
+    TX: {
+      parent_id: "TX",
+      x: "425",
+      y: "435"
+    },
+    NM: {
+      parent_id: "NM",
+      x: "305",
+      y: "365"
+    },
+    KS: {
+      parent_id: "KS",
+      x: "445",
+      y: "290"
+    },
+    NE: {
+      parent_id: "NE",
+      x: "420",
+      y: "225"
+    },
+    SD: {
+      parent_id: "SD",
+      x: "413",
+      y: "160"
+    },
+    ND: {
+      parent_id: "ND",
+      x: "416",
+      y: "96"
+    },
+    WY: {
+      parent_id: "WY",
+      x: "300",
+      y: "180"
+    },
+    MT: {
+      parent_id: "MT",
+      x: "280",
+      y: "95"
+    },
+    CO: {
+      parent_id: "CO",
+      x: "320",
+      y: "275"
+    },
+    UT: {
+      parent_id: "UT",
+      x: "223",
+      y: "260"
+    },
+    AZ: {
+      parent_id: "AZ",
+      x: "205",
+      y: "360"
+    },
+    NV: {
+      parent_id: "NV",
+      x: "140",
+      y: "235"
+    },
+    OR: {
+      parent_id: "OR",
+      x: "100",
+      y: "120"
+    },
+    WA: {
+      parent_id: "WA",
+      x: "130",
+      y: "55"
+    },
+    ID: {
+      parent_id: "ID",
+      x: "200",
+      y: "150"
+    },
+    CA: {
+      parent_id: "CA",
+      x: "79",
+      y: "285"
+    },
+    MI: {
+      parent_id: "MI",
+      x: "663",
+      y: "185"
+    },
+    PR: {
+      parent_id: "PR",
+      x: "620",
+      y: "545"
+    },
+    GU: {
+      parent_id: "GU",
+      x: "550",
+      y: "540"
+    },
+    VI: {
+      parent_id: "VI",
+      x: "680",
+      y: "519"
+    },
+    MP: {
+      parent_id: "MP",
+      x: "570",
+      y: "575"
+    },
+    AS: {
+      parent_id: "AS",
+      x: "665",
+      y: "580"
+    }
+  },
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
